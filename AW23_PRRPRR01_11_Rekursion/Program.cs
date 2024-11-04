625694436adb93e18b09be92ab2540cc6e7f9ebd
magisterblaman <hannes.blaman@lbs.se>
2024-11-04 14:42:48 +0100

    Rekursion

--- AW23_PRRPRR01_11_Rekursion/Program.cs
+++ AW23_PRRPRR01_11_Rekursion/Program.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace AW23_PRRPRR01_11_Rekursion {
 	internal class Program {
+		static int Factorial(int n) {
+			if (n <= 1) { // basfall
+				return 1;
+			} else {
+				return n * Factorial(n - 1);
+			}
+		}
+
 		static void Main(string[] args) {
+			Console.WriteLine(Factorial(-3));
 		}
 	}
 }
